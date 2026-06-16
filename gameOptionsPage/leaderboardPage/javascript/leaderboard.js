c9cf8035ce4bc3a05c1ddd8fffe0ee9e2a613131
23407 <23407je@hvhs.school.nz>
2026-06-16 15:14:28 +1200

    accessingInfo

--- gameOptionsPage/leaderboardPage/javascript/leaderboard.js
+++ gameOptionsPage/leaderboardPage/javascript/leaderboard.js
@@ -1,5 +1,7 @@
 const HTML_OUTPUT = document.getElementById("databaseOutput");
 function displayUsers() {
     GLOBAL_User = JSON.parse(localStorage.getItem("GLOBAL_User"))
-    HTML_OUTPUT.innerHTML = hi;
+    let top5 = firebase.database().ref('/geodash/Users/highscoreData/').set('value')
+    console.log("message: " + top5)
+    HTML_OUTPUT.innerHTML = top5;
 }
