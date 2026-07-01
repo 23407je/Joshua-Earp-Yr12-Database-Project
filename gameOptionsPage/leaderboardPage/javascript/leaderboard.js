e931366306fc0404e1dfc4f5f0ced60c634a7952
23407 <23407je@hvhs.school.nz>
2026-07-02 10:15:54 +1200

    actual_finshed_Project

--- gameOptionsPage/leaderboardPage/javascript/leaderboard.js
+++ gameOptionsPage/leaderboardPage/javascript/leaderboard.js
@@ -9,7 +9,7 @@ function sortGeodashData(snapshot) {
     let data = snapshot.val()
     const grab_Userdata = Object.entries(data);
     for (let i = 0; i < Math.min(grab_Userdata.length, 5); i++) {
-        geodash_leaderboardData_display += grab_Userdata[i][0] + " Highscore: " + grab_Userdata[i][1].Highscore + " <br> "
+        geodash_leaderboardData_display += grab_Userdata[i][0] + " Highscore: " + grab_Userdata[i][1].User_Highscore + " <br> "
     }
 }
 function sortJetpackmanData(jetpackmanData) {
@@ -18,7 +18,7 @@ function sortJetpackmanData(jetpackmanData) {
     console.log(jetpackman_grab_Userdata);
     let jetpackman_leaderboardData_display = "";
     for (let i = 0; i < Math.min(jetpackman_grab_Userdata.length, 5); i++) {
-        jetpackman_leaderboardData_display += jetpackman_grab_Userdata[i][0] + " Highscore: " + jetpackman_grab_Userdata[i][1].Highscore + " <br> "
+        jetpackman_leaderboardData_display += jetpackman_grab_Userdata[i][0] + " Highscore: " + jetpackman_grab_Userdata[i][1].User_Highscore + " <br> "
     }
     HTML_OUTPUT.innerHTML = "<h2>Geodash top 5 highscores:</h2>" + "<br> <br>" + geodash_leaderboardData_display + "<br> <br>" + "<h2>Jetpackman top 5 highscores:</h2>" + "<br> <br>" + jetpackman_leaderboardData_display
 }
